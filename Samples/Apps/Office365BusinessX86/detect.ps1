f2fa306d3696e5beb9d3b4cbb5bcc7ca83120b31
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2023-02-23 15:20:37 +0100

    update

--- Samples/Apps/Office365BusinessX86/detect.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ProgramName = "Office365Business"
-
-$ChocoPrg_Existing = C:\ProgramData\chocolatey\choco.exe list --localonly
-    if ($ChocoPrg_Existing -like "*$ProgramName*"){
-    Write-Host "Found it!"
-}
