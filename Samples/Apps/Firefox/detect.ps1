a274e8561b59549aad8216b4be89bdc2330bfeb2
JorgaWetzel <51820881+JorgaWetzel@users.noreply.github.com>
2023-03-03 08:29:05 +0100

    Update

---
+++ Samples/Apps/Firefox/detect.ps1
@@ -0,0 +1,7 @@
+﻿$ProgramName = "firefox"
+
+
+$ChocoPrg_Existing = C:\ProgramData\chocolatey\choco.exe list --localonly
+    if ($ChocoPrg_Existing -like "*$ProgramName*"){
+    Write-Host "Found it!"
+}
